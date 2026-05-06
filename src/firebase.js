cd0dfa0cdc9ffcad62c5538f828c02fddcabcb3a
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 13:20:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD_RiRoqFccROtU1zgvNXWFAO4rTYDMt24",
+  authDomain: "mock-test-platform-4295d.firebaseapp.com",
+  projectId: "mock-test-platform-4295d",
+  storageBucket: "mock-test-platform-4295d.firebasestorage.app",
+  messagingSenderId: "472963857360",
+  appId: "1:472963857360:web:dde2bcef807d1de828d7d5",
+  measurementId: "G-Y61XJRTYC6"
 };
 
 const app = initializeApp(firebaseConfig);
